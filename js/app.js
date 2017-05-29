bf5070b21e830af9de1ac36787134e5e2f5d143d
Kenneth Lindqvist <kenneth.lindqvist@gmail.com>
2017-05-29 23:06:14 +0300

    Viivästetty JS lataaminen

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
 // Vegånia JS
+fetchInject([
+  'https://npmcdn.com/bootstrap@4.0.0-alpha.6/dist/js/bootstrap.min.js'
+], fetchInject([
+  'https://cdn.jsdelivr.net/jquery/3.1.1/jquery.slim.min.js',
+  'https://npmcdn.com/tether@1.2.4/dist/js/tether.min.js'
+]));
